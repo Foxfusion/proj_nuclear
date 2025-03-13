331e0c3d76061081e1f3100c41c27d39712860c0
Foxfusion <wgfox@foxfusion.net>
2025-03-12 21:22:21 -0600

    Project Clear App Test page

---
+++ proj_nuclear/src/App.js
@@ -0,0 +1,29 @@
+//
+// File Name: App Test.jsx
+// Date Created: 03/12/2025
+// Date Modified: 03/12/2025
+// Author: William, Fox
+// Project Name: Project Nuclear
+// Description: Upgrade to modern web architecture - Upgrade Project BigSky to modern WebStack JavaScript and React
+// import statements:
+// function component:
+
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Project Nuclear</h1>
+                <br />
+                <h4> Author:  William Fox</h4>
+                <br />
+                <h5>Components Used:</h5>
+            </header>
+        </div>
+    );
+}
+
+export default App;
